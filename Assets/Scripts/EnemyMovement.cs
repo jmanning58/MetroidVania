6174d723ce7948f3d26011d6ffea6ffd125a4a70
jmanning58 <manningjc@g.cofc.edu>
2022-01-12 18:14:56 -0500

    Crab Movement and Sprite Flip on Collision

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -21,5 +21,26 @@ public class EnemyMovement : MonoBehaviour
     void Update()
     {
         myRigidbody.velocity = new Vector2 (moveSpeed, 0f);
+        Walk();
+    }
+
+    void Walk()
+    {
+        Vector2 crabVelocity = new Vector2(myRigidbody.velocity.x, myRigidbody.velocity.y);
+        myRigidbody.velocity = crabVelocity;
+
+        bool crabHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("isWalking", crabHasHorizontalSpeed);
+    }
+
+    void OnTriggerExit2D(Collider2D other) 
+    {
+        moveSpeed = -moveSpeed;
+        FlipEnemyFacing();    
+    }
+
+    void FlipEnemyFacing()
+    {
+        transform.localScale = new Vector2 (-(Mathf.Sign(myRigidbody.velocity.x)), 1f);
     }
 }
